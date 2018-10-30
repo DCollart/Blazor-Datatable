efac5b5607970919dbdce9dd877fe7e241123e47
Dominic Collart <dominic.collart@outlook.fr>
2018-10-30 22:06:29 +0100

    Add search on all items based on text criteria

--- Mithril.Blazor.Datatable/Components/Datatable.cshtml.cs
+++ Mithril.Blazor.Datatable/Components/Datatable.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Components;
 using System;
 using System.Collections.Generic;
@@ -22,12 +23,35 @@ namespace Mithril.Blazor.Datatable.Components
         [Parameter]
         protected Action<TItem> OnLineClick { get; set; }
 
-        protected IEnumerable<TItem> CurrentPage => Items.Page(PageSize, PageNumber);
+        protected IEnumerable<TItem> CurrentPage => GetCurrentPage();
+
+        protected string SearchCriteria { get; set; }
+
+        protected IEnumerable<TItem> GetCurrentPage()
+        {
+
+            if (!string.IsNullOrEmpty(SearchCriteria))
+            {
+                // I used IndexOf instead of Contains because of the ignore case option.
+                var items = Items.Where(i => Columns.Select(c => c.Content(i).ToString()).Any(c => c.IndexOf(SearchCriteria, StringComparison.OrdinalIgnoreCase) >= 0));
+                return items.Page(PageSize, PageNumber);
+            }   
+            else
+            {
+                return Items.Page(PageSize, PageNumber);
+            }
+        }
 
         protected void OnPageNumberChanged(int pageNumber)
         {
             PageNumber = pageNumber;
             this.StateHasChanged();
+        }
+
+        protected void OnSearchCriteraChange(UIChangeEventArgs changeEvent)
+        {
+            SearchCriteria = changeEvent.Value.ToString();
+            PageNumber = 1;
         }
     }
 }
